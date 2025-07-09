4afb106676b74644e6c4b274ffbf5986b9f5d82a
09wizardguy <09wizardguy@gmail.com>
2025-07-09 15:23:05 -0400

    Change methods

--- src/main/java/com/o9wizardguy/mixin/SaplingBlockMixin.java
+++ src/main/java/com/o9wizardguy/mixin/SaplingBlockMixin.java
@@ -3,8 +3,8 @@ package com.o9wizardguy.mixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SaplingBlock;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -14,27 +14,31 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SaplingBlock.class)
+@Mixin(value = SaplingBlock.class, priority = 2000)
 public abstract class SaplingBlockMixin {
-    private static final TagKey<Block> GREENHOUSE_BLOCKS = TagKey.of(RegistryKeys.BLOCK, new Identifier("demandinggreenhouses", "greenhouse_blocks"));
 
-    @Inject(method = "randomTick", at = @At("TAIL"))
+    private static final TagKey<Block> GREENHOUSE_BLOCKS =
+            TagKey.of(RegistryKeys.BLOCK, new Identifier("demandinggreenhouses", "greenhouse_blocks"));
+
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
     private void allowGrowthUnderGreenhouse(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
-        boolean greenhouseAbove = false;
+        // If the sky is blocked directly above
+        if (!world.isSkyVisible(pos.up())) {
+            // Check for greenhouse blocks 25–50 blocks above
+            for (int i = 25; i <= 50; i++) {
+                BlockPos checkPos = pos.up(i);
+                BlockState checkState = world.getBlockState(checkPos);
+                if (checkState.isIn(GREENHOUSE_BLOCKS)) {
+                    // Cancel the tick to prevent Demanding Saplings from killing the sapling
+                    ci.cancel();
 
-        for (int i = 25; i <= 50; i++) {
-            BlockPos checkPos = pos.up(i);
-            BlockState checkState = world.getBlockState(checkPos);
-            if (checkState.isIn(GREENHOUSE_BLOCKS)) {
-                greenhouseAbove = true;
-                break;
-            }
-        }
-
-        if (greenhouseAbove) {
-            SaplingBlock sapling = (SaplingBlock)(Object)this;
-            if (world.isAir(pos.up()) && sapling.canGrow(world, world.random, pos, state)) {
-                sapling.grow(world, world.random, pos, state);
+                    // Force sapling growth even though the sky is blocked
+                    SaplingBlock sapling = (SaplingBlock)(Object)this;
+                    if (world.isAir(pos.up()) && sapling.canGrow(world, random, pos, state)) {
+                        sapling.grow(world, random, pos, state);
+                    }
+                    return;
+                }
             }
         }
     }
