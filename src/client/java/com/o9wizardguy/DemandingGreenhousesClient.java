afb39ee1a818ff08883ed3f939d7e7629d70b17b
09wizardguy <09wizardguy@gmail.com>
2025-07-08 23:43:37 -0400

    Initial setup from fabric mod template generator + correct fabric.mod.json to have correct info

---
+++ src/client/java/com/o9wizardguy/DemandingGreenhousesClient.java
@@ -0,0 +1,10 @@
+package com.o9wizardguy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DemandingGreenhousesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
